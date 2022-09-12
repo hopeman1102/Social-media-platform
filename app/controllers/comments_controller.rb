e06026e240046746df91560283c9e2bb3bc0f8c5
hopeman1102 <hopeman1102@gmail.com>
2022-09-12 13:50:30 +0530

    modified functionalities. Removed all the functions here as comment functionalities needs authorization, so have these functionalities in application controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,2 @@
 class CommentsController < ApplicationController
-  def create
-    new_comment = Comment.new(comments_params)
-    if new_comment.save!
-      render status: 200, html: "Comment saved successfully"
-    else
-      render status: 400, html: "Not saved successfully"
-    end
-  end
-
-  private
-
-  def comments_params
-    params.require(:comment_data).permit(:user_id, :post_id, :content)
-  end
-
 end
