68e9a84abdfc1affca3932ddad72735f74199288
hopeman1102 <hopeman1102@gmail.com>
2022-09-12 13:51:06 +0530

    added funtionalities for users and posts controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,7 @@
 class PostsController < ApplicationController
-  def create 
-    new_post = Post.new(posts_params)
-    if new_post.save!
-      render status: 200, html: "post saved"
-    else
-      render status: 400, html: "post not saved"
-    end
-  end
-
-  private
-
-  def posts_params
-    params.require(:post_data).permit(:user_id, :content, :image)
+  def show
+    post = Post.find(params[:id])
+    render json: {post: post, comments: post.comments}
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,29 @@
-class UsersController < ApplicationController
-  def new
+class UsersController < ApplicationController  
+  def show
+    render json: User.find(params[:id]), status: 200
+  rescue ActiveRecord::RecordNotFound
+    render html: "User not exists", status: 400
   end
-  
+
   def create
     user = User.new(user_params)
     if user.save!
       render status: 200, html:"User saved"
     else
       render status: 400, html:"User not saved"
+    end
+  end
+
+  def log_in
+    user = User.find_by(user_name: params[:user_name])
+    if user.present? && user.authenticate(params[:password])
+      jwt = encode_token({"user_id": user.id, "expire": 24.hours.from_now}) 
+      #eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozfQ.zNubY6aofl2h7HaiXyKqiDwN2Ii_PgCnWa4pzJOv83I
+      #header.payload.signature
+      render status: 200, json: {token: jwt,  
+        username: user.user_name}
+    else
+      render status: 400, html: "Wrong pass or username"
     end
   end
 
