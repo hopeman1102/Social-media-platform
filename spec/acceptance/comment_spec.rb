5a3751813e8d544ba57bd2ef8e9008b951d5d85c
hopeman1102 <hopeman1102@gmail.com>
2022-09-14 12:03:22 +0530

    Added all the test cases with rspec. 3 spec files made for 3 models ie. User, Post, Comment

---
+++ spec/acceptance/comment_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource 'Comments' do
+  before do
+    @post_user= User.create(user_name: "test1", password: "12345", password_confirmation: "12345")
+    @comment_user = User.create(user_name: "test2", password: "12345", password_confirmation: "12345")
+    @post = Post.create(user_id: @post_user[:id],content: "This is the first post")
+    Post.create(user_id: @post_user[:id],content: "This is the second post")
+    Comment.create(user_id: @comment_user[:id], post_id: @post[:id], content: "This is a comment  on the first post")
+    Comment.create(user_id: @comment_user[:id], post_id: @post[:id], content: "This is second comment  on the first post")
+    header 'Authorization', :token
+  end
+  post "comments" do
+    before do
+      @comment_user = User.create(user_name: "test3", password: "12345", password_confirmation: "12345")
+    end
+    context "200" do
+      let(:token) {"bearer #{JWT.encode({user_id: @comment_user[:id]}, Rails.application.secrets.secret_key_base)}"}
+      it "Create a new comment" do
+        params_obj = {comment_data: {post_id: Post.first[:id], content: "New comment created"}}
+        do_request(params_obj)
+        expect(status).to eq(200)
+      end
+    end
+    context "400" do
+      let(:token) {"bearer #{JWT.encode({user_id: 0}, Rails.application.secrets.secret_key_base)}"}
+      it "Create a new comment with wrong token" do
+        params_obj = {comment_data: {post_id: @post[:id], content: "New comment created"}}
+        do_request(params_obj)
+        expect(status).to eq(400)
+      end
+    end
+    context "400" do
+      before do
+        @comment_user = User.create(user_name: "test3", password: "12345", password_confirmation: "12345")
+      end
+      let(:token) {"bearer #{JWT.encode({user_id: @comment_user[:id]}, Rails.application.secrets.secret_key_base)}"}
+      it "Create a new comment with wrong post id" do
+        params_obj = {comment_data: {post_id: 0, content: "New comment created"}}
+        do_request(params_obj)
+        expect(status).to eq(400)
+      end
+    end
+  end
+end
--- spec/acceptance/user_spec.rb
+++ spec/acceptance/user_spec.rb
@@ -5,7 +5,7 @@ resource 'Users' do
   before do
     User.create(user_name: "test1", password: "12345", password_confirmation: "12345")
   end
-  get 'user/:id' do
+  get 'users/:id' do
     context "200" do
       User.create(user_name: "test1", password: "12345", password_confirmation: "12345")
       x = User.first
@@ -23,48 +23,88 @@ resource 'Users' do
       end
     end
   end
-  get 'post/:id' do
-    context "200" do
+  get "my_posts", :type => :controller do
+    before do
       User.create(user_name: "test1", password: "12345", password_confirmation: "12345")
-      u = User.first
-      Post.create(user_id: u[:id],content: "This is the content of post")
-      x = Post.first
-      let(:id) {x[:id]}
-      it "Show post with valid id in the link" do
+      token = {user_id: User.first.id}
+      Post.create(user_id: token["user_id"], content: "Content")
+      header 'Authorization', "bearer #{JWT.encode(token, Rails.application.secrets.secret_key_base)}"
+    end
+    context "200" do 
+      it "Show posts of the logged in user" do
         do_request()
         expect(status).to eq(200)
       end
     end
-    context "400" do
-      let(:id) { 0 }
-      it "Show post with invalid id in the link" do
+    context "401" do
+      before do
+        header 'Authorization', 'bearer'
+      end
+      it "My post page not available for user who have not logged in" do
+        res = do_request()
+        expect(status).to eq(401)
+      end
+    end
+  end
+  get "users", :type => :controller do
+    before do
+      User.create(user_name: "test1", password: "12345", password_confirmation: "12345")
+      token = {user_id: User.first.id}
+      header 'Authorization', "bearer #{JWT.encode(token, Rails.application.secrets.secret_key_base)}"
+    end
+    context "200" do 
+      it "Show profile of the logged in user" do
+        do_request()
+        expect(status).to eq(200)
+      end
+    end
+    context "401" do 
+      before do
+        header 'Authorization', ""
+      end
+      it "Invalid profile for user not logged in" do
+        do_request()
+        expect(status).to eq(401)
+      end
+    end
+    context "400" do 
+      before do
+        token = {user_id: 0}
+        header 'Authorization', "bearer #{JWT.encode(token, Rails.application.secrets.secret_key_base)}"
+      end
+      it "Invalid profile for user not exists" do
         do_request()
         expect(status).to eq(400)
       end
     end
   end
-  get "my_posts" do
-    context "200" do 
-      User.create(user_name: "test1", password: "12345", password_confirmation: "12345")
-      token = {user_id: User.first.id}
-      headers['Authorization'] = JWT.encode token, Rails.application.secrets.secret_key_base
-      it "Show posts of the logged in user" do
-        # byebug
-        do_request()
+
+  post "users" do
+    before do
+      User.create(user_name: "alreadyauser", password: "123456", password_confirmation: "123456")
+    end
+    context "200" do
+      it "Create a new user" do
+        params_obj = {user_data: {name: "test",user_name: "test",
+          bio: "Hello everyone!! I'm test", password: "123456",password_confirmation: "123456"}}
+        do_request(params_obj)
         expect(status).to eq(200)
       end
     end
-  end
-
-  get "profile" do
-    context "200" do 
-      User.create(user_name: "test1", password: "12345", password_confirmation: "12345")
-      token = {user_id: User.first.id}
-      headers['Authorization'] = JWT.encode token, Rails.application.secrets.secret_key_base
-      it "Show profile of the logged in user" do
-        # byebug
-        do_request()
-        expect(status).to eq(200)
+    context "400" do
+      it "Create a new user wrong passwords" do
+        params_obj = {user_data: {name: "test",user_name: "test",
+          bio: "Hello everyone!! I'm test", password: "123",password_confirmation: "123456"}}
+        do_request(params_obj)
+        expect(status).to eq(400)
+      end
+    end
+    context "400" do
+      it "Create a new user with existing user name" do
+        params_obj = {user_data: {name: "alreadyauser",user_name: "alreadyauser",
+          bio: "Hello everyone!! I'm test", password: "123456", password_confirmation: "123456"}}
+        do_request(params_obj)
+        expect(status).to eq(400)
       end
     end
   end
