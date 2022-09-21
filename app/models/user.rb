851e16e4d2e3e9cf8ea1fc26b46805e6af7b264e
hopeman1102 <hopeman1102@gmail.com>
2022-09-21 13:23:57 +0530

    Added validations on email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_secure_password
 
   VALID_USERNAME_REGEX = /\A^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,20}\z/i
+  VALID_EMAIL_REGEX = /\A\s*[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\s*\z/i
   validates :user_name, presence: true, uniqueness: true, format: {with: VALID_USERNAME_REGEX}
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
   has_many :posts
   has_many :post_likes
 end
